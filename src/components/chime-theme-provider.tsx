1854d98c7e60e6c8ab242e08b993f9e51b89bc39
Dennis Martin Cual <denniscual0716@gmail.com>
2025-08-31 03:00:07 +0800

    create chime theme provider

---
+++ src/components/chime-theme-provider.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from 'amazon-chime-sdk-component-library-react';
+
+interface IProps {
+    children?: React.ReactNode;
+}
+
+const ChimeThemeProvider: React.FC<IProps> = ({ children }) => {
+    return <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>;
+};
+
+export default ChimeThemeProvider;
