a54acdef8fe1484eea5f60454e4c4cb1314925f9
Dennis Martin Cual <denniscual0716@gmail.com>
2025-08-31 11:20:00 +0800

    remove chime global styles due to the conflict with tailwindcss global

--- src/components/chime-theme-provider.tsx
+++ src/components/chime-theme-provider.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import {
-  GlobalStyles,
-  lightTheme,
-} from "amazon-chime-sdk-component-library-react";
+import { lightTheme } from "amazon-chime-sdk-component-library-react";
 
 interface IProps {
   children?: React.ReactNode;
 }
 
 const ChimeThemeProvider: React.FC<IProps> = ({ children }) => {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>;
 };
 
 export default ChimeThemeProvider;
