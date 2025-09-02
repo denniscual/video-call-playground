459747915406c30fd734555419b996ac46902140
Dennis Martin Cual <denniscual0716@gmail.com>
2025-09-02 15:42:23 +0800

    use aws rds as our db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,23 +5,23 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env.local" });
 
 if (
-	!process.env.AURORA_DB_NAME ||
-	!process.env.AURORA_SECRET_ARN ||
-	!process.env.AURORA_RESOURCE_ARN
+  !process.env.AURORA_DB_NAME ||
+  !process.env.AURORA_SECRET_ARN ||
+  !process.env.AURORA_RESOURCE_ARN
 ) {
-	throw new Error(
-		"AURORA_DB_NAME, AURORA_SECRET_ARN, and AURORA_RESOURCE_ARN must be set in your env file.",
-	);
+  throw new Error(
+    "AURORA_DB_NAME, AURORA_SECRET_ARN, and AURORA_RESOURCE_ARN must be set in your env file.",
+  );
 }
 
 export default defineConfig({
-	schema: "./src/lib/db/schema.ts", // path to your schema file(s)
-	out: "./drizzle", // where to output migration files
-	dialect: "postgresql",
-	driver: "aws-data-api",
-	dbCredentials: {
-		database: process.env.AURORA_DB_NAME,
-		secretArn: process.env.AURORA_SECRET_ARN,
-		resourceArn: process.env.AURORA_RESOURCE_ARN,
-	},
+  schema: "./src/lib/db/schema.ts", // path to your schema file(s)
+  out: "./drizzle", // where to output migration files
+  dialect: "postgresql",
+  driver: "aws-data-api",
+  dbCredentials: {
+    database: process.env.AURORA_DB_NAME,
+    secretArn: process.env.AURORA_SECRET_ARN,
+    resourceArn: process.env.AURORA_RESOURCE_ARN,
+  },
 });
--- src/lib/db/clients/aws-rds.ts
+++ src/lib/db/clients/aws-rds.ts
@@ -3,30 +3,34 @@ import { RDSDataClient } from "@aws-sdk/client-rds-data";
 import * as schema from "../schema";
 
 if (!process.env.AURORA_DB_NAME) {
-	throw new Error(
-		"AURORA_DB_NAME is not set. Please add your AWS RDS connection database name to .env.local",
-	);
+  throw new Error(
+    "AURORA_DB_NAME is not set. Please add your AWS RDS connection database name to .env.local",
+  );
 }
 
 if (!process.env.AURORA_SECRET_ARN) {
-	throw new Error(
-		"AURORA_SECRET_ARN is not set. Please add your AWS RDS secret arn to .env.local",
-	);
+  throw new Error(
+    "AURORA_SECRET_ARN is not set. Please add your AWS RDS secret arn to .env.local",
+  );
 }
 
 if (!process.env.AURORA_RESOURCE_ARN) {
-	throw new Error(
-		"AURORA_RESOURCE_ARN is not set. Please add your AWS RDS resource arn to .env.local",
-	);
+  throw new Error(
+    "AURORA_RESOURCE_ARN is not set. Please add your AWS RDS resource arn to .env.local",
+  );
 }
 
 const rdsClient = new RDSDataClient({
-	region: process.env.NEXT_PUBLIC_AURORA_REGION,
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
 });
 
 export const rdsDbClient = drizzle(rdsClient, {
-	database: process.env.AURORA_DB_NAME,
-	secretArn: process.env.AURORA_SECRET_ARN,
-	resourceArn: process.env.AURORA_RESOURCE_ARN,
-	schema,
+  database: process.env.AURORA_DB_NAME,
+  secretArn: process.env.AURORA_SECRET_ARN,
+  resourceArn: process.env.AURORA_RESOURCE_ARN,
+  schema,
 });
